fd125d259b8d7688335c5dcbfe9337bc12a063c0
hosu <hojune0904@gmail.com>
2023-01-25 19:06:30 +0900

    Add pass job and tasklet

--- src/main/java/com/hosu/pass/PassBatchApplication.java
+++ src/main/java/com/hosu/pass/PassBatchApplication.java
@@ -2,8 +2,9 @@ package com.hosu.pass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PassBatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(PassBatchApplication.class, args);
